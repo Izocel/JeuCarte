b296ce464a421aaeec54e4443b3331dc797c5665
Izocel <68454661+Izocel@users.noreply.github.com>
2022-01-28 21:13:54 -0500

    initial commit

---
+++ JeuCarte/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JeuCarte
+//
+//  Created by user193538 on 1/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, blob!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JeuCarte/JeuCarteApp.swift
@@ -0,0 +1,17 @@
+//
+//  JeuCarteApp.swift
+//  JeuCarte
+//
+//  Created by user193538 on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct JeuCarteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
